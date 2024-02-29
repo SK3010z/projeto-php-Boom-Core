2be9547a9c90edb5ca51194a8eae74acfafd46db
SK3010z <sk3010mfc@gmail.com>
2024-02-28 22:58:28 -0300

    Boom Core v0.06

--- Boom Core/index.php
+++ Boom Core/index.php
@@ -8,15 +8,21 @@
 </head>
 <body>
     <div id="header">
-        <span class="logo"><a class="logo" href="./"><img src="boom core.png" alt=""></a></span>
-        <span id="boom"><a href="./">Boom Core</a></span>
-        <a id="produtos" href="./" onmouseover="start('traco1')" onmouseleave="end('traco1')"><p>Produtos</p><div id="traco1"></div></a>
-        <a id="servicos" href="./" onmouseover="start('traco2')" onmouseleave="end('traco2')"><p>Serviços</p><div id="traco2"></div></a>
-        <a id="atendimentos" href="./" onmouseover="start('traco3')" onmouseleave="end('traco3')"><p>Atendimentos</p><div id="traco3"></div></a>
-        <form action="index.php" id="pesquisa" method="get">
-            <input type="text" placeholder="Pesquisa" name="pesquisa">
-            <input type="submit" id="botao" value="">
-        </form>
+        <span id="headerL">
+            <span class="logo"><a class="logo" href="./"><img src="boom core.png" alt=""></a></span>
+            <span id="boom"><a href="./">Boom Core</a></span>
+        </span>
+        <span id="headerR">
+            <a id="produtos" href="./" onmouseover="start('traco1')" onmouseleave="end('traco1')"><p>Produtos</p><div id="traco1"></div></a>
+            <a id="servicos" href="./" onmouseover="start('traco2')" onmouseleave="end('traco2')"><p>Serviços</p><div id="traco2"></div></a>
+            <a id="atendimentos" href="./" onmouseover="start('traco3')" onmouseleave="end('traco3')"><p>Atendimentos</p><div id="traco3"></div></a>
+            <form action="index.php" id="pesquisa" method="get">
+                <input type="text" placeholder="Pesquisa" name="pesquisa">
+                <input type="submit" id="botao" value="" title="pesquisar">
+            </form>
+            <a href="index.php"><img id="user" src="user.png" title="perfil do usuário"/></a>
+            <a href="index.php"><img id="notificacao" src="sino.png" title="perfil do usuário"/></a>
+        </span>
     </div>
 
     <script src="script.js"></script>

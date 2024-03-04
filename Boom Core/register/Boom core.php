6f2147a7e4bd6f6a753224c17b68f88dfc3eec3f
SK3010z <sk3010mfc@gmail.com>
2024-03-04 16:46:17 -0300

    Boom Core v0.07

---
+++ Boom Core/register/Boom core.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" type="text/css" href="Boom.css" />
+  </head>
+  <body>
+    
+    <div class="flex_container">
+      
+      <form method="get">
+        <h1>Cadastro</h1>
+        <!-- Nome  -->
+        <div class="form1">
+          <label for="exampleInputEmail1" class="form-label">Nome</label>
+          <input type="name" class="form-control" id="exampleInputEmail1" />
+        </div>
+        <br />
+        <!-- Email -->
+        <div class="form1">
+          <label for="exampleInputPassword1" class="form-label">Email</label>
+          <input type="email" class="form-control" id="exampleInputPassword1" />
+        </div>
+        <br />
+        <!-- Senha  -->
+        <div class="form1">
+          <label for="exampleInputPassword1" class="form-label">Senha</label>
+          <input
+            type="password"
+            class="form-control"
+            id="exampleInputPassword1"
+          />
+        </div>
+        <br />
+        <!-- enviar -->
+        <button type="submit" id="botaoEnviar">Enviar</button>
+      </form>
+    </div>
+  </body>
+</html>
--- Boom Core/register/index.php
+++ Boom Core/register/index.php
@@ -8,14 +8,16 @@
 </head>
 <body>
     <div id="container">
+        <div id="content">
         <h1>Cadastro</h1>
-        <form action="./">
-            <input type="text" name="nome" id="nome"><br>
-            <input type="email" name="email" id="email"><br>
-            <input type="password" name="senha" id="senha"><br>
-            <input type="submit" value="Criar conta"><br>
-
-        </form>
+            <form action="./">
+                
+                <input type="text" name="nome" id="nome"><br>
+                <input type="email" name="email" id="email"><br>
+                <input type="password" name="senha" id="senha"><br>
+                <input type="submit" value="Criar conta"><br>
+            </form>
+        </div>
     </div>
 </body>
 </html>

49a76202a53bd5f63888f5ff7acbe9dfdfb6d44f
SK3010z <sk3010mfc@gmail.com>
2024-03-05 11:00:32 -0300

    Boom Core v0.09

--- Boom Core/registro/index.php
+++ Boom Core/registro/index.php
@@ -2,35 +2,45 @@
 <html>
   <head>
     <meta charset="utf-8" />
-    <link rel="stylesheet" type="text/css" href="Boom.css" />
+    <link rel="stylesheet" type="text/css" href="style.css" />
   </head>
   <body>
-    
+
+
+  <div id="header">
+        <span id="headerL">
+            <span class="logo"><a class="logo" href="./"><img src="../assets/boom core.png" alt=""></a></span>
+            <span id="boom"><a href="./">Boom Core</a></span>
+        </span>
+    </div>
+
+
     <div class="flex_container">
       
-      <form method="get">
+      <form method="post" action="index.php">
         <h1>Cadastro</h1>
         <!-- Nome  -->
-        <div class="form1">
+        <!-- <div class="form1"> -->
           <label for="exampleInputEmail1" class="form-label">Nome</label>
-          <input type="name" class="form-control" id="exampleInputEmail1" />
-        </div>
+          <input type="name" class="form-control" id="exampleInputEmail1" name="nome"/>
+        <!-- </div> -->
         <br />
         <!-- Email -->
-        <div class="form1">
+        <!-- <div class="form1"> -->
           <label for="exampleInputPassword1" class="form-label">Email</label>
-          <input type="email" class="form-control" id="exampleInputPassword1" />
-        </div>
+          <input type="email" class="form-control" id="exampleInputPassword1" name="email"/>
+        <!-- </div> -->
         <br />
         <!-- Senha  -->
-        <div class="form1">
+        <!-- <div class="form1"> -->
           <label for="exampleInputPassword1" class="form-label">Senha</label>
           <input
             type="password"
             class="form-control"
             id="exampleInputPassword1"
+            name="senha"
           />
-        </div>
+        <!-- </div> -->
         <br />
         <!-- enviar -->
         <button type="submit" id="botaoEnviar">Enviar</button>

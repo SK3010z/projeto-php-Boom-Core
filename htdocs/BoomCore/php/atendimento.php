f77867d61aac824bac6efe69ec61e3a61a33cf66
SK3010z <sk3010mfc@gmail.com>
2024-04-14 22:03:11 -0300

    atendimentos banco de dados

--- htdocs/BoomCore/php/atendimento.php
+++ htdocs/BoomCore/php/atendimento.php
@@ -1,3 +1,16 @@
+<?php
+
+if(isset($_POST['enviar'])){
+  include("assets/conn.php"); /* atendimento -> id, nome, email, assunto, mensagem*/
+  $nome = $_POST['nome'];
+  $email = $_POST['email'];
+  $assunto = $_POST['assunto'];
+  $mensagem = $_POST['mensagem'];
+  $sql = "INSERT INTO atendimento(nome,email,assunto,mensagem) VALUES('$nome','$email','$assunto','$mensagem');";
+  $conn -> query($sql);
+}
+
+?>
 <html>
 
 <head>
@@ -18,14 +31,15 @@
       <div class="camp">
         <!-- <legend>Campo de atendimento</legend><br> -->
         <label for="nome">Nome:</label><br>
-        <input type="text" id="nome" name="nome" class="entrada"><br><br>
+        <input type="text" id="nome" name="nome" class="entrada" required maxlength="50"><br><br>
         <label for="gmail">Email:</label><br>
-        <input type="email" id="gmail" name="gmail" class="entrada"><br><br>
+        <input type="email" id="gmail" name="email" class="entrada" required maxlength="70"><br><br>
         <label for="assunto">Assunto:</label><br>
-        <input type="text" id="assunto" name="assunto" class="entrada"><br><br>
+        <input type="text" id="assunto" name="assunto" class="entrada" required maxlength="40"><br><br>
         <label for="mensag">Mensagem:</label><br>
-        <textarea id="mensag" name="mensag" rows="8" cols="40" class="entrada"></textarea><br><br>
+        <textarea id="mensag" name="mensagem" rows="8" cols="40" class="entrada" required maxlength="500"></textarea><br><br>
         <button type="submit" id="enviar" name="enviar">Enviar</button>
+        
     </Form>
   </div>
   </div>

897926e56ba2378d93c8f0146d3816072318a4c3
SK3010z <sk3010mfc@gmail.com>
2024-06-22 22:59:14 -0300

    aparecer tela de indisponivel depois do serviço caso não tenha nenhum funcionário disponível

--- htdocs/BoomCore/php/assets/header.php
+++ htdocs/BoomCore/php/assets/header.php
@@ -155,7 +155,6 @@ STYLE
 </style>
 
 <script src="../js/header.js">
-
 </script>
 <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
 <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
--- htdocs/BoomCore/php/servicos.php
+++ htdocs/BoomCore/php/servicos.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
 include("./assets/conn.php");
+
+$confirmacao = false;
 if (isset($_COOKIE["session"])) {
   $session_Cookie = explode(" ", $_COOKIE["session"]);
   $_SESSION["user"] = $session_Cookie[0];
@@ -12,6 +14,38 @@ if (isset($_COOKIE["session"])) {
 if (!isset($_SESSION['user'])) {
   setcookie("naoLogado", "servicos.php"); //sessão
   header("location: login.php");
+}
+
+if (isset($_POST["submitServico"])) {
+  // ocupado, nome_cliente, email_cliente, telefone_cliente, endereco_cliente, dispositivo_cliente, problema_cliente
+  $confirmacao = true;
+  $nome_cliente = $_POST["nome_cliente"];
+  $email_cliente = $_POST["email_cliente"];
+  $telefone_cliente = $_POST["telefone_cliente"];
+  $endereco_cliente = $_POST["endereco_cliente"];
+  $dispositivo_cliente = $_POST["dispositivo_cliente"];
+  $problema_cliente = $_POST["problema_cliente"];
+
+  $selectFuncionariosSQL = "SELECT * FROM servicos WHERE ocupado = 'NAO'";
+  $selectFuncionarios = $conn->query($selectFuncionariosSQL);
+  $funcionario = mysqli_fetch_assoc($selectFuncionarios);
+  if (mysqli_num_rows($selectFuncionarios) > 0) {
+    $nomeFuncionario = $funcionario["nome"];
+    $idFuncionario = $funcionario["id"];
+
+    $insertServicoSQL = "UPDATE servicos SET
+      ocupado = 'SIM',
+      nome_cliente = '{$nome_cliente}',
+      email_cliente = '{$email_cliente}',
+      telefone_cliente = {$telefone_cliente},
+      endereco_cliente = '{$endereco_cliente}',
+      dispositivo_cliente = '{$dispositivo_cliente}',
+      problema_cliente = '{$problema_cliente}'
+      WHERE id = {$idFuncionario};";
+
+    $conn->query($insertServicoSQL);
+  }
+  unset($_POST);
 }
 
 ?>
@@ -37,22 +71,22 @@ if (!isset($_SESSION['user'])) {
       <h1 style="font-size: 1.7em;" id="servico"></h1>
       <form action="" method="post">
         <label for="nome">Nome Completo</label>
-        <input type="text" id="name" name="name" required>
+        <input type="text" id="name" name="nome_cliente" required>
 
         <label for="email">E-mail</label>
-        <input type="email" id="email" name="email" required>
+        <input type="email" id="email" name="email_cliente" required>
 
-        <label for="tele">Telefone</label>
-        <input type="tel" id="phone" name="phone" required>
+        <label for="tele">Whatsapp</label>
+        <input type="tel" id="phone" name="telefone_cliente" required>
 
         <label for="ender">Endereço</label>
-        <input type="text" id="address" name="address" required>
+        <input type="text" id="address" name="endereco_cliente" required>
 
         <label for="disp">Dispositivo</label>
-        <input type="text" id="device" name="device" required>
+        <input type="text" id="device" name="dispositivo_cliente" required>
 
         <label for="descr">Descrição do Problema</label>
-        <textarea id="issue" name="issue" rows="4" required></textarea>
+        <textarea id="issue" name="problema_cliente" rows="4" required></textarea>
 
         <button type="submit" name="submitServico">Enviar Solicitação</button>
 
@@ -61,6 +95,7 @@ if (!isset($_SESSION['user'])) {
     <div class="confirmacao" style="display: none;">
       <img src="https://cdn-icons-png.flaticon.com/512/1004/1004739.png">
       <h2>Serviço confirmado!</h2>
+      <p id="responsavel"></p>
       <!--colocar a tela inicial do site-->
       <button id="okConfimacao">OK</button>
     </div>
@@ -109,7 +144,7 @@ if (!isset($_SESSION['user'])) {
         <button class="contratar" tipoServico="Formatação">Contratar</button>
       </div>
     </div>
-    <div class="serv">
+    <!-- <div class="serv">
       <img src="../images/montagem.png" />
       <div class="descr">
         <h2>Montagem de PC</h2>
@@ -118,7 +153,7 @@ if (!isset($_SESSION['user'])) {
         <button class="contratar" tipoServico="Montagem de PC">Contratar</button>
       </div>
     </div>
-  </div>
+  </div> -->
 
 
 </body>
@@ -130,10 +165,30 @@ if (!isset($_SESSION['user'])) {
 <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
 <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 <?php
-if (isset($_POST["submitServico"])) {
+if ($confirmacao) {
   echo <<<HTML
-    <script>confirmacao()</script>
+    <script>
+    
+    </script>
     HTML;
+  if (isset($nomeFuncionario)) {
+    echo <<<HTML
+        <script>
+          confirmacao();
+          $("#responsavel").html("<b style='font-weight: 700;'>{$nomeFuncionario}</b> irá ficar responsável pelo seu serviço");
+        </script>
+      HTML;
+  } else {
+    echo <<<HTML
+        <script>
+          confirmacao();
+          $(".confirmacao h2").text("Indisponível");
+          $("#responsavel").text("Não há nenhuma pessoa que possa ficar responsável pelo serviço.");
+          $(".confirmacao img").attr("src", "https://cdn-icons-png.flaticon.com/512/3239/3239888.png");
+        </script>
+      HTML;
+  }
+  
 }
 
 ?>
